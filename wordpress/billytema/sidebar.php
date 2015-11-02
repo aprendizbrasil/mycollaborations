d6b7e01af61cdf1528506d8a96d89f87a6504810
aprendizbrasil <aprendizbrasil@hotmail.com>
2015-11-02 01:44:30 -0200

    Create sidebar.php

---
+++ wordpress/billytema/sidebar.php
@@ -0,0 +1,10 @@
+<!--Tema: Billy Tema-->
+<!--Fonte: https://www.youtube.com/watch?v=44XAFLCBo9g-->
+
+<!-- Código que exibe a Sidebar   -->
+
+<div class="sidebar">
+	<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar()) : ?>
+	
+	<?php endif; ?>
+</div>
